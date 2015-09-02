d4b5f8eed5190da9d489a722a30805fd7b538735
cajacko <charlie@Charless-MacBook-Pro.local>
2015-09-02 09:02:52 +0100

    Twitter link in sidebar opens in new tab

--- sections/sidebar.php
+++ sections/sidebar.php
@@ -1,5 +1,5 @@
 <section id="sidebar" class="absolute-sidebar">
 	<div id="sidebar-container">
-		<a class="twitter-timeline" href="https://twitter.com/charliejackson" data-widget-id="461551137443704833"><div id="twitter-placeholder"><i class="fa fa-twitter"></i> @charliejackson</div></a>
+		<a class="twitter-timeline" href="https://twitter.com/charliejackson" data-widget-id="461551137443704833" target="_blank"><div id="twitter-placeholder"><i class="fa fa-twitter"></i> @charliejackson</div></a>
 	</div>
 </section>

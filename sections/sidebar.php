3fdc68717e001aab55c39fe6b8dd3f3274d85f5b
cajacko <charlie@Charless-MacBook-Pro.local>
2015-09-02 09:00:59 +0100

    Improved look of Twitter in the sidebar
    Formatted the Twitter link in the sidebar for when the Twitter
    JavaScript hasn’t loaded

--- sections/sidebar.php
+++ sections/sidebar.php
@@ -1,5 +1,5 @@
 <section id="sidebar" class="absolute-sidebar">
 	<div id="sidebar-container">
-		<a class="twitter-timeline" href="https://twitter.com/charliejackson" data-widget-id="461551137443704833">Tweets by @charliejackson</a>
+		<a class="twitter-timeline" href="https://twitter.com/charliejackson" data-widget-id="461551137443704833"><div id="twitter-placeholder"><i class="fa fa-twitter"></i> @charliejackson</div></a>
 	</div>
 </section>

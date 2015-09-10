1ce8923870b28180239ce384fc9f3c93273437cb
cajacko <charlie@Charless-MacBook-Pro.local>
2015-09-10 16:47:31 +0100

    Added GitHub links

--- sections/site-navigation.php
+++ sections/site-navigation.php
@@ -25,6 +25,7 @@
 						<ul class="sub-nav">
 							<li><a target="_blank" href="<?php echo get_template_directory_uri(); ?>/inc/media/cv.pdf">CV</a></li>
 							<li><a href="<?php echo esc_url( home_url() ); ?>/portfolio">Portfolio</a></li>
+							<li><a target="_blank" href="https://github.com/cajacko">GitHub</a></li>
 							<li><a target="_blank" href="http://uk.linkedin.com/in/charliejackson9">LinkedIn</a></li>
 							<li><a href="mailto:contact@charliejackson.com">Email Me</a></li>
 						</ul>
@@ -36,6 +37,7 @@
 						<ul class="sub-nav">
 							<li><a target="_blank" href="http://uk.linkedin.com/in/charliejackson9">LinkedIn</a></li>
 							<li><a target="_blank" href="https://twitter.com/charliejackson">Twitter</a></li>
+							<li><a target="_blank" href="https://github.com/cajacko">GitHub</a></li>
 							<li><a target="_blank" href="https://uk.pinterest.com/cajacko/">Pinterest</a></li>
 							<li><a target="_blank" href="https://www.youtube.com/c/CharlieJackson9">YouTube</a></li>
 							<li><a href="mailto:contact@charliejackson.com">Email Newsletter</a></li>

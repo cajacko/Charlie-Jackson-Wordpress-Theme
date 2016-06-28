abbc2b9c704721bd6923de31d9cf2661f079e7cf
Charlie Jackson <charlie@charliejackson.com>
2016-06-28 07:50:25 +0100

    Added tweets source

--- prototype/public/index.php
+++ prototype/public/index.php
@@ -5,5 +5,40 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('../');
 $twig = new Twig_Environment($loader);
 $template = $twig->loadTemplate('layouts/loop.twig');
+
+// $tweets = file_get_contents('../data/twitter-user-profile.json');
+// $tweets = json_decode($tweets, true);
+
+// switch (json_last_error()) {
+//     case JSON_ERROR_NONE:
+//         echo ' - No errors';
+//     break;
+//     case JSON_ERROR_DEPTH:
+//         echo ' - Maximum stack depth exceeded';
+//     break;
+//     case JSON_ERROR_STATE_MISMATCH:
+//         echo ' - Underflow or the modes mismatch';
+//     break;
+//     case JSON_ERROR_CTRL_CHAR:
+//         echo ' - Unexpected control character found';
+//     break;
+//     case JSON_ERROR_SYNTAX:
+//         echo ' - Syntax error, malformed JSON';
+//     break;
+//     case JSON_ERROR_UTF8:
+//         echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+//     break;
+//     default:
+//         echo ' - Unknown error';
+//     break;
+// }
+
+// echo PHP_EOL;
+
+// // var_dump($tweets); exit;
+// exit;
+
+
+
 $content = $template->render(array());
 echo $content;
